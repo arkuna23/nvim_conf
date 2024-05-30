d34bd6ca307be1e141336aa371b8f9c28876ce25
naraku_night <nightalpha23@gmail.com>
2024-05-30 13:50:40 +0800

    cmp tab key

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -420,6 +420,22 @@ plugins["nvim-autopairs"] = {
 	opts = {},
 }
 
+plugins["LuaSnip"] = {
+	"L3MON4D3/LuaSnip",
+-- stylua: ignore
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+}
+
 plugins["nvim-cmp"] = {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
